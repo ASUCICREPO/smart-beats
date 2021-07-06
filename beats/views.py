1f63542670d5112ed58086a92ad1beec6a4cd9a5
Risabh Raj <rraj21@asu.edu>
2021-07-06 11:00:51 -0700

    Fix shapefile and crime_data upload form

--- beats/views.py
+++ beats/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from beats.custom_storage import ShapefileStorage, CrimeDataStorage
 from .forms import CityForm
 from .models import City
 
@@ -13,51 +12,12 @@ def upload(request):
         form = CityForm(request.POST, request.FILES)
 
         if form.is_valid():
-            shape_file = request.FILES['shape_file']
-            crime_data = request.FILES['crime_data']
-
-            shape_file_bucket = ShapefileStorage()
-            crime_data_bucket = CrimeDataStorage()
-
-            shape_file_bucket.save(shape_file.name, shape_file)
-            crime_data_bucket.save(crime_data.name, crime_data)
-
             form.save()
             return redirect('beats_list')
     else:
         form = CityForm()
 
     return render(request, 'beats/upload.html', {'form': form})
-
-
-def upload_shapefile(request):
-    context = {}
-
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-
-        media_storage = ShapefileStorage()
-        # Get updated name of file in case of same-name upload
-        name = media_storage.save(uploaded_file.name, uploaded_file)
-        context['url'] = media_storage.url(name)
-
-    return render(request, 'beats/upload.html', context)
-
-
-def upload_crime_data(request):
-    context = {}
-
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-
-        media_storage = CrimeDataStorage()
-        print(media_storage.bucket_name)
-
-        # Get updated name of file in case of same-name upload
-        name = media_storage.save(uploaded_file.name, uploaded_file)
-        context['url'] = media_storage.url(name)
-
-    return render(request, 'beats/upload.html', context)
 
 
 def beats_list(request):
