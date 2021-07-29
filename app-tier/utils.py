3d78b308fd971cfcccf3ca151b43746ee124681a
Risabh Raj <rraj21@asu.edu>
2021-07-29 11:33:42 -0700

    Delete output beat files after sending response

--- app-tier/utils.py
+++ app-tier/utils.py
@@ -1,5 +1,8 @@
-# Logging
 import logging
+import os
+from time import sleep
+
+import settings as s
 
 
 def init_logger(name):
@@ -13,3 +16,15 @@ def init_logger(name):
         datefmt='%Y-%m-%d %H:%M:%S',
     )
     return logger
+
+
+def delete_file(file_path):
+    print(f'Deleting output beats file {file_path} after 10s')
+    sleep(10)
+
+    for ext in s.shapefile_components:
+        if os.path.exists(file_path + ext):
+            os.remove(file_path + ext)
+
+    if os.path.exists(file_path + '.zip'):
+        os.remove(file_path + '.zip')
