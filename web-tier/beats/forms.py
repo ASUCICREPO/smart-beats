01926e41124c5b6a8b9ae1f92bad4c6961f5f6f2
risabhrizz <rraj21@asu.edu>
2021-09-03 00:27:56 -0700

    Change date field to datetime

--- web-tier/beats/forms.py
+++ web-tier/beats/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from .models import City, Crime
 
 
@@ -15,17 +14,29 @@ class BeatGenerateForm(forms.ModelForm):
         ('ATTRIBUTE_TARGET', 'Calls for Service'),
         ('NUMBER_ZONES_AND_ATTRIBUTE', 'Number of Beats')
     ]
+
+    DISPOSITION_CHOICES = [
+        ('initial', 'Select'),
+        (1, 'Field Interview (1)'),
+        (2, 'False Alarm (2)'),
+        (3, 'Unable to locate (3)'),
+        (5, 'Assist Fire Department (5)'),
+        (6, 'Report (6)'),
+        (9, 'Contact made (9)'),
+    ]
+
     beat_creation_method = forms.ChoiceField(choices=BEAT_CREATION_CHOICES)
+    disposition_type = forms.ChoiceField(choices=DISPOSITION_CHOICES)
     cfs_per_beat = forms.IntegerField(label='Calls for Service per Beat', required=False)
     number_of_beats = forms.DecimalField(required=False)
-    start_datetime = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
-    end_datetime = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+    start_datetime = forms.DateTimeField(widget=forms.NumberInput(attrs={'type': 'datetime-local'}))
+    end_datetime = forms.DateTimeField(widget=forms.NumberInput(attrs={'type': 'datetime-local'}))
 
     class Meta:
         model = Crime
         fields = (
-            'priority', 'is_incident', 'beat_creation_method', 'cfs_per_beat', 'number_of_beats', 'start_datetime',
-            'end_datetime')
+            'priority', 'is_incident', 'disposition_type', 'beat_creation_method', 'cfs_per_beat', 'number_of_beats',
+            'start_datetime', 'end_datetime')
 
     class Media:
         js = ('beats/js/main.js',)
--- web-tier/beats/utils.py
+++ web-tier/beats/utils.py
@@ -52,20 +52,26 @@ def upload_file_to_s3(filepath, object_name):
 def get_filtered_crime_geo_dataframe(payload, city_obj):
     priority_list = payload['priority']
     is_incident = payload['is_incident']
+    disposition_type = payload['disposition_type']
 
     sd = payload['start_datetime']
-    start_datetime = datetime.datetime(sd.year, sd.month, sd.day, tzinfo=pytz.timezone('US/Arizona'))
+    start_datetime = datetime.datetime(sd.year, sd.month, sd.day, sd.hour, sd.minute, tzinfo=pytz.timezone('US/Arizona'))
     ed = payload['end_datetime']
-    end_datetime = datetime.datetime(ed.year, ed.month, ed.day, tzinfo=pytz.timezone('US/Arizona'))
+    end_datetime = datetime.datetime(ed.year, ed.month, ed.day, ed.hour, ed.minute, tzinfo=pytz.timezone('US/Arizona'))
 
+    # Add check for time range to query
     query = Q(timestamp__gte=start_datetime) & Q(timestamp__lte=end_datetime)
 
+    # Add check for Priority info and incident status to query
     if priority_list and is_incident:
         query &= Q(priority__in=priority_list) & Q(is_incident=is_incident)
     elif priority_list:
         query &= Q(priority__in=priority_list)
     elif is_incident:
         query &= Q(is_incident=is_incident)
+
+    # Add check for Disposition field
+    query &= Q(disposition=disposition_type)
 
     query_res = Crime.objects.filter(query).values()
     logger.info(f"Total rows received: {len(query_res)}")
